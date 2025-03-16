73e92978b0c3ddd32ff5615051efe3ec746f6d94
Kaan Uzdoğan <kaanuzdogan@hotmail.com>
2025-03-16 21:44:54 +0100

    Add Matrix chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import EventsContainer from "@/components/EventsContainer";
 import { EventType } from "@/app/views/Schedule";
 import { useEffect, useState } from "react";
 import { LuCalendarPlus } from "react-icons/lu";
+import { SiMatrix } from "react-icons/si";
 
 interface Question {
   id: string;
@@ -123,7 +124,7 @@ export default function Home() {
               <h2 className="text-sm md:text-base text-primary-500 font-medium">June 8-22</h2>
             </div>
           </div>
-          <div className="flex items-center gap-4">
+          <div className="flex items-center gap-2 sm:gap-4">
             <a
               href="https://forms.fillout.com/t/6zcH1pfc4ius"
               target="_blank"
@@ -137,6 +138,7 @@ export default function Home() {
               href="https://forms.fillout.com/t/6zcH1pfc4ius"
               target="_blank"
               rel="noopener noreferrer"
+              title="Submit your event"
               className="px-2 py-2 bg-primary-500 rounded-lg text-white font-medium hover:opacity-90 transition-opacity flex md:hidden items-center gap-2 md:text-base text-sm"
             >
               <LuCalendarPlus className="w-5 h-5" />
@@ -145,11 +147,21 @@ export default function Home() {
               href="https://github.com/blockchainweek/2025"
               target="_blank"
               rel="noopener noreferrer"
+              title="View the source code"
               className="text-gray-400 hover:text-white transition-colors"
             >
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                 <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z" />
               </svg>
+            </a>
+            <a
+              href="https://matrix.to/#/%23blockchainweek:dod.ngo"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="text-gray-400 hover:text-white transition-colors"
+              title="Join our Matrix chat"
+            >
+              <SiMatrix className="w-6 h-6" />
             </a>
           </div>
         </div>
